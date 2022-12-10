afcc6794d78b7d2b5e98863b83c2cce83ac0a9a7
igorqueirozz <igorqueirozz@outlook.com>
2022-12-09 22:28:20 -0300

    New methods to add answers and categories added on view model.

--- app/src/main/java/dev/queiroz/quizdatagenerator/activity/NewQuizViewModel.kt
+++ app/src/main/java/dev/queiroz/quizdatagenerator/activity/NewQuizViewModel.kt
@@ -4,36 +4,76 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.queiroz.quizdatagenerator.model.Answer
 import dev.queiroz.quizdatagenerator.model.Category
+import dev.queiroz.quizdatagenerator.model.Question
 import javax.inject.Inject
 
 @HiltViewModel
-class NewQuizViewModel @Inject constructor(): ViewModel() {
+class NewQuizViewModel @Inject constructor() : ViewModel() {
     private val _stepper = MutableLiveData(0)
     val stepper: LiveData<Int> = _stepper
 
     private val _categories = MutableLiveData(mutableListOf<Category>())
     val categories: LiveData<MutableList<Category>> = _categories
 
-    fun nextStep(){
+    private val _questions = MutableLiveData(mutableListOf<Question>())
+    val questions: LiveData<MutableList<Question>> = _questions
+
+    private val _answers = MutableLiveData(mutableListOf<Answer>())
+    val answers: LiveData<MutableList<Answer>> = _answers
+
+    private val _alertMessage = MutableLiveData<String>()
+    val alertMessage: LiveData<String> = _alertMessage
+
+    fun nextStep() {
         if (_stepper.value == 0) _stepper.value = (_stepper.value!! + 1)
     }
 
-    fun backStep(){
-         _stepper.value = (_stepper.value!! - 1)
-        if(_stepper.value!! < 0) _stepper.value = 0
+    fun backStep() {
+        _stepper.value = (_stepper.value!! - 1)
+        if (_stepper.value!! < 0) _stepper.value = 0
     }
 
-    fun addCategory(category: Category){
+    fun addCategory(category: Category) {
         val list = categories.value
         list?.add(category)
         _categories.value = list
     }
 
-    fun removeCategory(index: Int){
+    fun removeCategory(index: Int) {
         val list = categories.value
         list?.removeAt(index)
         _categories.value = list
+    }
+
+    fun addAnswer(answer: Answer) {
+        val list = answers.value
+        list?.add(answer)
+        _answers.value = list
+    }
+
+    fun addQuestion(questionText: String, source: String, category: Category) {
+        if (answers.value!!.isEmpty()) {
+            _alertMessage.value = "You must add some answer."
+        } else {
+            val list = _questions.value
+            list?.add(
+                Question(
+                    id = null,
+                    category = category,
+                    questionText = questionText,
+                    source = source,
+                    answers = answers.value!!
+                )
+            )
+            _questions.value = list
+        }
+
+    }
+
+    fun clearAlertMessage(){
+        _alertMessage.value = ""
     }
 
 }
--- app/src/main/java/dev/queiroz/quizdatagenerator/ui/fragment/quiz/NewQuizStep2Fragment.kt
+++ app/src/main/java/dev/queiroz/quizdatagenerator/ui/fragment/quiz/NewQuizStep2Fragment.kt
@@ -6,33 +6,140 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import dev.queiroz.quizdatagenerator.R
 import dev.queiroz.quizdatagenerator.activity.NewQuizViewModel
 import dev.queiroz.quizdatagenerator.databinding.FragmentNewQuizStep2Binding
+import dev.queiroz.quizdatagenerator.model.Answer
+import dev.queiroz.quizdatagenerator.model.Category
+import dev.queiroz.quizdatagenerator.ui.fragment.quiz.adapter.AnswerRecyclerViewAdapter
+import dev.queiroz.quizdatagenerator.ui.fragment.quiz.adapter.CategoryRecyclerViewAdapter
 
 class NewQuizStep2Fragment : Fragment() {
     private lateinit var binding: FragmentNewQuizStep2Binding
     private lateinit var categoryOptions: AutoCompleteTextView
+    private lateinit var answerRecyclerView: RecyclerView
     private val newQuizViewModel: NewQuizViewModel by activityViewModels()
     private val listOfCategories by lazy {
         newQuizViewModel.categories.value
     }
+    private var categorySelected: Category? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentNewQuizStep2Binding.inflate(inflater, container, false)
+        answerRecyclerView = binding.recyclerAnswers
         setCategoriesOptions()
+        setListeners()
+        setObservers()
+        setupRecyclerView()
         return binding.root
     }
 
-    private fun setCategoriesOptions(){
+    private fun setCategoriesOptions() {
         val itemsLabel = listOfCategories?.map { category -> category.description }!!.toList()
         val adapter = ArrayAdapter(requireContext(), R.layout.simple_item_option_list, itemsLabel)
         categoryOptions = ((binding.categoriesOptions as? AutoCompleteTextView)!!)
         categoryOptions.setAdapter(adapter)
+        categoryOptions.setOnItemClickListener { adapterView, _, position, _ ->
+            categorySelected =
+                listOfCategories?.find {
+                    it.description == adapterView.getItemAtPosition(position).toString()
+                }
+        }
+    }
+
+    private fun setListeners() {
+        binding.run {
+            buttonAddAnswer.setOnClickListener { validateAndAddAnswer() }
+            buttonSaveQuestion.setOnClickListener { validateAndAddQuestion() }
+        }
+    }
+
+    private fun setObservers() {
+        newQuizViewModel.alertMessage.observe(viewLifecycleOwner) {
+            if (!it.isNullOrBlank()) {
+                Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+                newQuizViewModel.clearAlertMessage()
+            }
+        }
+    }
+
+    private fun validateAndAddAnswer() {
+        val answerText = binding.tieAnswer.text.toString()
+        val isCorrect = binding.cbIsCorrect.isChecked
+        if (answerText.isNullOrBlank()) {
+            Toast.makeText(context, "Answer field cannot be empty.", Toast.LENGTH_SHORT).show()
+        } else {
+            newQuizViewModel.addAnswer(
+                Answer(
+                    id = null,
+                    description = answerText,
+                    isCorrect = isCorrect
+                )
+            )
+            binding.run {
+                tieAnswer.text = null
+                cbIsCorrect.isChecked = false
+            }
+        }
+    }
+
+    private fun validateAndAddQuestion() {
+        val questionText = binding.tieQuestion.text.toString()
+        val questionSource = binding.tieAnswerSource.text.toString()
+
+        if (questionText.isNullOrBlank()) {
+            Toast.makeText(context, "Question field cannot be empty.", Toast.LENGTH_SHORT)
+                .show()
+        } else if (categorySelected == null) {
+            Toast.makeText(
+                context,
+                "You must have to select some category.",
+                Toast.LENGTH_SHORT
+            )
+                .show()
+        } else {
+            newQuizViewModel.addQuestion(
+                questionText = questionText,
+                source = questionSource,
+                category = categorySelected!!
+            )
+            binding.run {
+                tieQuestion.text = null
+                tieAnswerSource.text = null
+            }
+        }
+    }
+
+    private fun setupRecyclerView() {
+        answerRecyclerView.layoutManager = LinearLayoutManager(context)
+        answerRecyclerView.adapter = CategoryRecyclerViewAdapter(listOf())
+
+//        val swipeToDeleteCallback = object : SwipeToDeleteCallback(requireContext()){
+//            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+//                val position = viewHolder.adapterPosition
+//                newQuizViewModel.removeCategory(position)
+//                answerRecyclerView.adapter?.notifyItemRemoved(position)
+//            }
+//        }
+
+//        val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallback)
+//        itemTouchHelper.attachToRecyclerView(categoriesRecyclerView)
+//
+        newQuizViewModel.answers.observe(viewLifecycleOwner) {
+            with(answerRecyclerView) {
+                setHasFixedSize(true)
+                adapter = AnswerRecyclerViewAdapter(it)
+                adapter?.notifyDataSetChanged()
+            }
+        }
     }
 
 
